209ca15871d3cc829fc0b19e9b49af5b4999e4a4
LAPTOP-PUU0AVTP\siddh <shuklaelec20082007@gmail.com>
2025-05-21 16:16:57 +0530

    Code refactored for user controller.

--- server.js
+++ server.js
@@ -2,15 +2,26 @@ const express = require('express');
 const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/dbConnection');
+const userRoutes = require('./routes/user/userRoutes');
+const employeeRoutes = require('./routes/employee/employeeRoutes');     
+const employerRoutes = require('./routes/employer/employerRoutes');
+const contactRoutes = require('./routes/contacts/contactRoutes');
+
+
 const app = express();
+
 const port = process.env.PORT || 5000;
+
 connectDB(); // Connect to MongoDB
+
 app.use(express.json());
-app.use('/api/contacts', require('./routes/contacts/contactRoutes'));
-app.use('/api/v1/auth', require('./routes/user/userRoutes'));
-app.use('/api/v1/employees', require('./routes/employee/employeeRoutes'));
-app.use('/api/v1/employers', require('./routes/employer/employerRoutes'));
+app.use('/api/contacts', contactRoutes);
+app.use('/api/v1/auth', userRoutes);
+app.use('/api/v1/employees', employeeRoutes);
+app.use('/api/v1/employers', employerRoutes);  
+
 app.use(errorHandler);
+
 app.listen(port, () => {               
     console.log(`Server is running on port ${port}`);
 });
