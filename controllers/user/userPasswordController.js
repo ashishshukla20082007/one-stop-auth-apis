e9d623887f713ef024d72c4b6c659c141ec9aed9
LAPTOP-PUU0AVTP\siddh <shuklaelec20082007@gmail.com>
2025-05-23 18:03:31 +0530

    Fixed to send token in the notification api.

--- controllers/user/userPasswordController.js
+++ controllers/user/userPasswordController.js
@@ -23,6 +23,9 @@ const userForgotPassword = asyncHandler(async (req, res) => {
   
       // 4. Send raw token (not hashed) in reset link
       const resetLink = `http://localhost:5001/api/v1/auth/reset-password/${encodeURIComponent(rawToken)}`;
+
+      // Get token from header                    
+      const authToken = req.headers.authorization.split(" ")[1];
       
       // 5. Send notification through notification service
       await axios.post('http://localhost:5002/notify/email', {
@@ -30,6 +33,10 @@ const userForgotPassword = asyncHandler(async (req, res) => {
         to: user.email,
         subject: 'Password Reset',
         text: `<p>Click <a href="${resetLink}">here</a> to reset your password. The link is valid for 1 hour.</p>`
+      }, {
+        headers: {
+          'Authorization': `Bearer ${authToken}`
+        }
       });
       
       res.status(200).json({ message: 'Password reset email sent' });
